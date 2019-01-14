2c6d6d72e3a69a53122ba39b51eebaeaf0b01b90
joseEuclide <46220170+joseEuclide@users.noreply.github.com>
2019-01-14 22:22:02 +0000

    Add files via upload

---
+++ MakeItHappen1Application.java
@@ -0,0 +1,14 @@
+package com.euclidesMakeithappen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeItHappen1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeItHappen1Application.class, args);
+	}
+
+}
+
